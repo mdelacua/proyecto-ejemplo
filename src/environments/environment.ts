7851ecf610652bf5ac22896653351801e0099f65
mdelacua <marcos.delacuadra95@gmail.com>
2023-05-17 21:59:37 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'proyecto-ejemplo-fb',
+    appId: '1:806677470972:web:82a752b99e2d55ae36c179',
+    storageBucket: 'proyecto-ejemplo-fb.appspot.com',
+    apiKey: 'AIzaSyB-jVQqFOkHKUUuQ_EwhQkDsDq3xBe3Sv0',
+    authDomain: 'proyecto-ejemplo-fb.firebaseapp.com',
+    messagingSenderId: '806677470972',
+  },};
